b9cd4f7f80a941be918042668bf09b319bf04dcc
Nada-09 <nadaalmajidi4@gmail.com>
2024-09-30 11:46:19 +0300

    Initial Commit

---
+++ HomePage/HomePageApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomePageApp.swift
+//  HomePage
+//
+//  Created by Nada Abdullah on 27/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct HomePageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HomePageTests/HomePageTests.swift
@@ -0,0 +1,17 @@
+//
+//  HomePageTests.swift
+//  HomePageTests
+//
+//  Created by Nada Abdullah on 27/03/1446 AH.
+//
+
+import Testing
+@testable import HomePage
+
+struct HomePageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
