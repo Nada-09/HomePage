3e44dda2752b9480a7e9b3c61e16fc6452c4e161
Nada-09 <nadaalmajidi4@gmail.com>
2024-10-02 10:36:05 +0300

    h

--- HomePage/ContentView.swift
+++ HomePage/ContentView.swift
@@ -15,40 +15,46 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack {
-            
-            VStack{
-    
-                ScrollView{
-                    
-                    HStack{
-                        ReminderStatView(icon: "calendar.badge.clock", title: "Today",count: 0)
-                        ReminderStatView(icon: "square.stack", title: "All",count: 0)
+            //ZStack{
+                //Color.black.ignoresSafeArea()
+                
+                //VStack{
+                    VStack{
+                        ScrollView{
+                            HStack {
+                                ReminderStatView(icon: "calendar.badge.clock", title: "Today",count: 0)
+                                ReminderStatView(icon: "square.stack", title: "All",count: 0)
+                            }
+                            HStack{
+                                ReminderStatView(icon: "checklist", title: "Doing",count: 0)
+                                ReminderStatView(icon: "checkmark", title: "Done",count: 0)
+                            }
+                            HStack{
+                                ReminderStatView(icon: "trash", title: "Deleted",count: 0)
+                                ReminderStatView(icon: "plus.rectangle", title: "Add List")
+                            }
+                            
+                            Spacer()
+                            
+                            
+                            Button{
+                                isPresented = true
+                            } label: {
+                                Text(Image(systemName: "plus.circle.fill"))
+                                    .frame(maxWidth: .infinity,alignment: .bottom)
+                                    .font(.title)
+                                    .controlSize(ControlSize.extraLarge)
+                            }.padding()
+                        }
                     }
-                    HStack{
-                        ReminderStatView(icon: "checklist", title: "Doing",count: 0)
-                        ReminderStatView(icon: "checkmark", title: "Done",count: 0)
-                    }
-                    HStack{
-                        ReminderStatView(icon: "trash", title: "Deleted",count: 0)
-                        ReminderStatView(icon: "plus.rectangle", title: "Add List")
-                    }
+                    .navigationTitle("Zenith")
+                    .searchable(text: $search, prompt: "Search")
                     
-                    Spacer()
-                    
-                    
-                    Button{
-                        isPresented = true
-                    } label: {
-                        Text(Image(systemName: "plus.circle.fill"))
-                            .frame(maxWidth: .infinity,alignment: .bottom)
-                            .font(.title)
-                            .controlSize(ControlSize.extraLarge)
-                    }.padding()
                 }
             }
-            
         }
-    }}
+    //}vstack
+//}zstack
 
 #Preview {
     ContentView()
